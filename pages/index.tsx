deb484159c830b6b5c3900ce267823bb611c4ac8
SahutoL <96646448+SahutoL@users.noreply.github.com>
2023-04-06 20:16:16 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ const App = () => {
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
-			<Header />
 			<TodoContents/>
 		</>
 	);
