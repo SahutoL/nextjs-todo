5ee31093cb2486bab3d40c13f3643536ae8c1b1c
SahutoL <greef.pandl@gmail.com>
2023-04-06 14:02:22 +0900

    installed vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/css/reset.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+	  <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
